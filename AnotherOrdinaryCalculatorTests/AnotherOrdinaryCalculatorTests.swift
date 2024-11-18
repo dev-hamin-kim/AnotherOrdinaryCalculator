a7b6ba1cc1188be7f0048d79157203d44dc11c4f
Hamin Kim <dev.hamin.kim@gmail.com>
2024-11-18 11:18:26 +0900

    Initial Commit

---
+++ AnotherOrdinaryCalculatorTests/AnotherOrdinaryCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherOrdinaryCalculatorTests.swift
+//  AnotherOrdinaryCalculatorTests
+//
+//  Created by 김하민 on 11/18/24.
+//
+
+import Testing
+@testable import AnotherOrdinaryCalculator
+
+struct AnotherOrdinaryCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

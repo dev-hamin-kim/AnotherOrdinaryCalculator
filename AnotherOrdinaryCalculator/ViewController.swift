485033bdfd132a3ea00c94781a0f3531b2da3e6e
Hamin Kim <dev.hamin.kim@gmail.com>
2024-11-19 14:27:25 +0900

    [new] initial commit deleted main storyboard imported SnapKit finished Lv. 1 requirements

--- AnotherOrdinaryCalculator/ViewController.swift
+++ AnotherOrdinaryCalculator/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
 
+    private func configureUI() {
+        
+        view.backgroundColor = .black
+        
+        label.text = "12345"
+        label.font = .boldSystemFont(ofSize: 60)
+        label.textColor = .white
+        label.textAlignment = .right
+        
+        view.addSubview(label)
+
+        label.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.top.equalToSuperview().inset(200)
+            make.height.equalTo(100)
+        }
+        
+    }
 
 }
 
+#Preview {
+    ViewController()
+}
